2da625db0f66b3811a8d343898e5ffe9ad828c3e
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2023-01-12 21:29:04 +0100

    Add StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle='Homepage'>
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage src='https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg'alt='The image of a brown lady'/>
       </Layout>
       
     </main>
